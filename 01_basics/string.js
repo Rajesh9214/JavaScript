b0db324454886c44cc1267fe4ec0728456b01435
Rajesh9214 <94107372+Rajesh9214@users.noreply.github.com>
2023-12-18 19:34:26 +0000

    String

---
+++ 01_basics/string.js
@@ -0,0 +1,13 @@
+const name = "Rajesh";
+const age = 28;
+
+// console.log(name + age ); dont use this type of syntax bcos this old dated 
+
+// In concatination we should use backticks (``) 
+
+console.log(`Hello my name is ${name} and my age is ${age}`); // this is better and readable code & 
+                                                            // this is called as string Interpolation
+
+// If we want to display name in upper case
+
+console.log(`Hello my name is ${name.toUpperCase()} and my age is ${age}`); // We can do this 
